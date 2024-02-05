602ca957e07b6309d9dfd5cf68f9a85099a3ff81
Aayaan <66046533+AayaanN@users.noreply.github.com>
2024-02-05 17:38:53 -0500

    Update views.py

--- flask_backend/api/views.py
+++ flask_backend/api/views.py
@@ -19,7 +19,7 @@ from pytz import timezone
 
 main = Blueprint('main', __name__)
 
-@main.route('/add_patients', methods=['POST'])
+@main.route('/add_patient', methods=['POST'])
 def add_patient():
     request_data = request.json
     name = request_data['name']
